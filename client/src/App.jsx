f99a93b9027c3bed5339d79bc467286ca68d776c
AjayPrajapathi <ajayprajapathi4@gmail.com>
2024-11-19 22:46:46 +0530

    folder structure completed

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+function App() {
+  return (
+   <div></div>
+  )
+}
+
+export default App
